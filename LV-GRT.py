93cb6ef13eabbedae9e167dd05275105f24b3735
AdrianoAlmiron <almironadriano89@gmail.com>
2022-07-26 15:26:47 -0300

    LV-GRT

---
+++ LV-GRT.py
@@ -0,0 +1,254 @@
+
+import pygame, random
+
+WIDTH = 720
+HEIGHT = 1080
+BLACK = (0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = (0, 255, 0)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode(
+    (WIDTH, HEIGHT),
+    pygame.FULLSCREEN
+)
+pygame.display.set_caption("LV-GRT")
+clock = pygame.time.Clock()
+
+def draw_text(surface, text, size, x, y):
+	font = pygame.font.SysFont("Roboto", size)
+	text_surface = font.render(text, True, BLACK)
+	text_rect = text_surface.get_rect()
+	text_rect.midtop = (x, y)
+	surface.blit(text_surface, text_rect)
+
+def draw_shield_bar(surface, x, y, percentage):
+	BAR_LENGHT = 300
+	BAR_HEIGHT = 20
+	fill = (percentage / 100) * BAR_LENGHT
+	border = pygame.Rect(x, y, BAR_LENGHT, BAR_HEIGHT)
+	fill = pygame.Rect(x, y, fill, BAR_HEIGHT)
+	pygame.draw.rect(surface, GREEN, fill)
+	pygame.draw.rect(surface, WHITE, border, 2)
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.image.load("assets/player.png").convert()
+		self.image.set_colorkey(BLACK)
+		self.rect = self.image.get_rect()
+		self.rect.centerx = WIDTH // 2
+		self.rect.bottom = HEIGHT - 10
+		self.speed_x = 0
+		self.shield = 100
+
+	def update(self):
+		self.speed_x = 0
+		self.speed_y = 0
+		keystate = pygame.key.get_pressed()
+		if keystate[pygame.K_LEFT]:
+			self.speed_x = -7
+		if keystate[pygame.K_RIGHT]:
+			self.speed_x = 7
+		if keystate[pygame.K_UP]:
+			self.speed_y = -5
+		if keystate[pygame.K_DOWN]:
+			self.speed_y = 5
+		self.rect.x += self.speed_x
+		self.rect.y += self.speed_y
+		if self.rect.right > WIDTH:
+			self.rect.right = WIDTH
+		if self.rect.left < 0:
+			self.rect.left = 0
+
+	def shoot(self):
+		bullet = Bullet(self.rect.centerx, self.rect.top)
+		all_sprites.add(bullet)
+		bullets.add(bullet)
+		laser_sound.play()
+
+	def shoot2(self):
+		bullet = Bullet(self.rect.centerx, self.rect.bottom)
+		all_sprites.add(bullet)
+		bullets.add(bullet)
+  		
+
+class Meteor(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = random.choice(meteor_images)
+		self.image.set_colorkey(BLACK)
+		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(WIDTH - self.rect.width)
+		self.rect.y = random.randrange(-140, -100)
+		self.speedy = random.randrange(1, 10)
+		self.speedx = random.randrange(-1, 1)
+
+	def update(self):
+		self.rect.y += self.speedy
+		self.rect.x += self.speedx
+		if self.rect.top > HEIGHT + 10 or self.rect.left < -40 or self.rect.right > WIDTH + 40:
+			self.rect.x = random.randrange(WIDTH - self.rect.width)
+			self.rect.y = random.randrange(-140, - 100)
+			self.speedy = random.randrange(1, 10)
+
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		self.image = pygame.image.load("assets/bullet.png")
+		self.image.set_colorkey(BLACK)
+		self.rect = self.image.get_rect()
+		self.rect.y = y
+		self.rect.centerx = x
+		self.speedy = -20
+  		
+
+	def update(self):
+		self.rect.y += self.speedy
+		if self.rect.bottom < 0:
+			self.kill()
+
+class Explosion(pygame.sprite.Sprite):
+	def __init__(self, center):
+		super().__init__()
+		self.image = explosion_anim[0]
+		self.rect = self.image.get_rect()
+		self.rect.center = center 
+		self.frame = 0
+		self.last_update = pygame.time.get_ticks()
+		self.frame_rate = 20 # VELOCIDAD DE LA EXPLOSION
+
+	def update(self):
+		now = pygame.time.get_ticks()
+		if now - self.last_update > self.frame_rate:
+			self.last_update = now
+			self.frame += 1
+			explosion_sound.play()
+			if self.frame == len(explosion_anim):
+				self.kill()
+			else:
+				center = self.rect.center
+				self.image = explosion_anim[self.frame]
+				self.rect = self.image.get_rect()
+				self.rect.center = center
+
+
+def show_go_screen():
+	screen.blit(background, [0,0])
+	draw_text(screen, "LV-GRT", 100, WIDTH // 2, HEIGHT // 4)
+	draw_text(screen, "SHOOT = SPACE", 27, WIDTH // 2, HEIGHT // 2)
+	draw_text(screen, "MOVE = ARROW", 27, WIDTH // 2, HEIGHT // 2.5)
+	draw_text(screen, "Press Any Key", 50, WIDTH // 2, HEIGHT * 3/4)
+	pygame.display.flip()
+	waiting = True
+	while waiting:
+		clock.tick(60)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			if event.type == pygame.KEYUP:
+				waiting = False
+
+
+meteor_images = []
+meteor_list = ["assets/enemy1.png", "assets/enemy2.png", "assets/enemy3.png", "assets/enemy4.png", 
+               "assets/enemy5.png", "assets/enemy6.png"]
+for img in meteor_list:
+	meteor_images.append(pygame.image.load(img).convert())
+
+
+####----------------EXPLOSTION IMAGENES --------------
+explosion_anim = []
+for i in range(9):
+	file = "assets/regularExplosion0{}.png".format(i)
+	img = pygame.image.load(file).convert()
+	img.set_colorkey(BLACK)
+	img_scale = pygame.transform.scale(img, (70,70))
+	explosion_anim.append(img_scale)
+
+# Cargar imagen de fondo
+background = pygame.image.load("assets/sky.jpg").convert()
+
+
+# Cargar sonidos
+laser_sound = pygame.mixer.Sound("assets/disparo.mp3")
+explosion_sound = pygame.mixer.Sound("assets/explosion.mp3")
+shoot_sound = pygame.mixer.Sound("assets/shoot.ogg")
+music_war = pygame.mixer.Sound("assets/shoot.ogg")
+pygame.mixer.music.load("assets/music.ogg")
+pygame.mixer.music.set_volume(0.4)
+music_war.play()
+
+pygame.mixer.music.play(loops=-1)
+
+
+#### ----------GAME OVER
+game_over = True
+running = True
+while running:
+	if game_over:
+
+		show_go_screen()
+
+		game_over = False
+		all_sprites = pygame.sprite.Group()
+		meteor_list = pygame.sprite.Group()
+		bullets = pygame.sprite.Group()
+
+		player = Player()
+		all_sprites.add(player)
+		for i in range(8):
+			meteor = Meteor()
+			all_sprites.add(meteor)
+			meteor_list.add(meteor)
+
+		score = 0
+
+
+	clock.tick(60)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				player.shoot()
+				player.shoot2()
+
+
+	all_sprites.update()
+
+	#colisiones - meteoro - laser
+	hits = pygame.sprite.groupcollide(meteor_list, bullets, True, True)
+	for hit in hits:
+		score += 100
+		#explosion_sound.play()
+		explosion = Explosion(hit.rect.center)
+		all_sprites.add(explosion)
+		meteor = Meteor()
+		all_sprites.add(meteor)
+		meteor_list.add(meteor)
+
+	# Checar colisiones - jugador - meteoro
+	hits = pygame.sprite.spritecollide(player, meteor_list, True)
+	for hit in hits:
+		player.shield -= 25
+		meteor = Meteor()
+		all_sprites.add(meteor)
+		meteor_list.add(meteor)
+		if player.shield <= 0:
+			game_over = True
+
+	screen.blit(background, [0, 0])
+
+	all_sprites.draw(screen)
+
+	#Marcador
+	draw_text(screen, str(score), 100, WIDTH // 2, 10)
+
+	# Escudo.
+	draw_shield_bar(screen, 5, 5, player.shield)
+
+	pygame.display.flip()
+pygame.quit()
